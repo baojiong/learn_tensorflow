ec43e8786101177db2f0bbfae96a9a5df52bf4c0
baojiong <baojiong@gmail.com>
2018-05-18 10:24:08 +0800

    update save_test.py

--- save_and_restore/save_test.py
+++ save_and_restore/save_test.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 
 v1 = tf.Variable(tf.constant(1.0, shape=[1]), name="v1")
 v2 = tf.Variable(tf.constant(1.0, shape=[1]), name="v2")
+result = v1 + v2
 
 init_op = tf.global_variables_initializer()
 
